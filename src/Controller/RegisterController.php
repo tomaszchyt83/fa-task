985fa773e87062be8bd6012664c10e01cc6c56cc
Tomasz Chytkowski <tomasz.chyt83@gmail.com>
2023-10-03 19:24:08 +0200

    User register on website.

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserRegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Controller used to register user.
+ */
+#[Route('/')]
+final class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'register', defaults: ['_format' => 'html'], methods: ['GET', 'POST'])]
+    public function register(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator,
+        UserPasswordHasherInterface $passwordHasher,
+        string $_format): Response
+    {
+        $user = new User();
+
+        $form = $this->createForm(UserRegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $errors = $validator->validate($user);
+
+            if (count($errors) > 0) {
+                $errorsString = (string) $errors;
+
+                return new Response($errorsString);
+            }
+
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
+            $user->setPassword($hashedPassword);
+
+            $user->setRoles([User::ROLE_ADMIN]);
+
+            $entityManager->persist($user);
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'user.registered');
+
+            return $this->redirectToRoute('admin_post_index');
+        }
+        return $this->render('user/register.'.$_format.'.twig', [
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/Form/UserRegisterType.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * Defines the form used to register an user.
+ */
+final class UserRegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'label.username'
+            ])
+            ->add('fullName', TextType::class, [
+                'label' => 'label.fullname',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'label.email',
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'label.password',
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(
+                        min: 10,
+                        max: 20,
+                    ),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
