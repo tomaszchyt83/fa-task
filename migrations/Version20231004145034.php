dfe30291287a8a74e6ecd4e0e94ab5e85083fc60
Tomasz Chytkowski <tomasz.chyt83@gmail.com>
2023-10-04 18:31:06 +0200

    Add description to migration.

--- migrations/Version20231004145034.php
+++ migrations/Version20231004145034.php
@@ -14,7 +14,7 @@ final class Version20231004145034 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add pricing plan column to user';
     }
 
     public function up(Schema $schema): void
